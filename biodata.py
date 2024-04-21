db95266a4ab5abd5ada5042a0a43b731a40b4654
sahrilnizam <145944627+sahrilnizam@users.noreply.github.com>
2024-04-21 23:09:35 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,18 @@
+#buat program untuk menampilkan biodata
+
+
+nama=(".m Sahril nizam.")
+nim=(".TI20230028.")
+semester=("dua")
+umur=(".20.")
+alamat=(".mertakmas.")
+lulusan=(".sma syarif imamuzzahidin.")
+
+print("=="*10)
+print("selamat datang", nama)
+print("nim anda", nim)
+print("anda semester",semester)
+print("umur anda", umur)
+print("alamat anda", alamat)
+print("lulusan sekolah", lulusan)
+print("=="*10)
